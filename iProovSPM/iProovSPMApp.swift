47bf822c78dd0e66ed00f12f9a7554ea0edad68a
Johan Sellström <johan.sellstrom@gmail.com>
2023-02-10 17:28:52 +0100

    Initial Commit

---
+++ iProovSPM/iProovSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  iProovSPMApp.swift
+//  iProovSPM
+//
+//  Created by Johan Sellström on 2023-02-10.
+//
+
+import SwiftUI
+
+@main
+struct iProovSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
